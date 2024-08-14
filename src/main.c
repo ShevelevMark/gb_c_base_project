73fab555faa6616988636936f77f2d7c73176536
ShevelevMark <shevelev.mm@yandex.ru>
2024-08-14 22:35:35 +0300

    Форматированный вывод передан в специальные функции

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <analysis.h>
+#include <write.h>
 
 int main(int argc, char **argv) {
 #ifdef _DEBUG
@@ -7,20 +7,13 @@ int main(int argc, char **argv) {
 #else
     printf("RELEASE BUILD!\n");
 #endif
-    
     YearStatistics_t ys = analysis(stdin);
-    if (ys.no_data) {
-        printf("No data in a file\n");
-    } else {
-        printf("Year %4d: {avg %2d; min %2d; max %2d; error lines %6u;}\n", ys.year, ys.avg, ys.min, ys.max, ys.err_cnt);
-        for (unsigned m = 0; m != 12; ++m)
-            if (ys.month[m].no_data) {
-                printf("Month %2d No data\n", m + 1);
-            } else {
-                MonthStatistics_t ms = ys.month[m];
-                printf("Month %2d: {avg %2d; min %2d; max %2d; error lines %6u;}\n", m + 1, ms.avg, ms.min, ms.max, ms.err_cnt);
-            }
-    }
+#ifdef _DEBUG
+    bool write_success = write_year(stdout, ys);
+    fprintf(stderr, "FORMAT WRITE %s\n", write_success ? "SUCCESS" : "FAIL");
+#else
+    write_year(stdout, ys);
+#endif
 
     return 0;
 }
