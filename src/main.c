38f16278810c6ff8422dd2630c988f36ee6212ff
mposimba <mposimba@MacBook-Pro-Mark.local>
2024-08-15 18:42:48 +0300

    Добавлена проверка ошибок повторного включения флагов и / или их отстутсвия

--- src/main.c
+++ src/main.c
@@ -8,7 +8,9 @@
 
 static void print_help(void) {
     printf("Statistic application expects command line options:\n");
-    printf("    -h prints this message\n    -f <filename> required, name of a file to read data from\n    -m <dd> optional, number of a month to print statistics of\n");
+    printf("    -h prints help message\n");
+    printf("    -f <filename> required (only once), name of a file to read data from\n");
+    printf("    -m <dd> optional (only once), number of a month to print statistics\n");
 }
 
 int main(int argc, char **argv) {
@@ -20,45 +22,62 @@ int main(int argc, char **argv) {
 
     char *filename = NULL;
     FILE *stream = NULL;
-    bool need_print_help = false;
+    int month_num = 0;
+    bool help_opt_set = false, filename_opt_set = false, month_opt_set = false;
     { 
         int opt_code = -1;
         while (-1 != (opt_code = getopt(argc, argv, "hf:m:"))) {
             switch(opt_code) {
                 case 'h':
-                    need_print_help = true;
+                    help_opt_set = true;
                     goto END; 
                 case 'f':
-                    if (NULL != filename) {
-                        printf("This option can not be used multiple times!\nApplication terminated!\n");
+                    if (filename_opt_set) {
+                        printf("-f option can not be set multiple times\nApplication terminated\n");
+                        help_opt_set = true;
                         goto END; 
                     }
                     filename = malloc(strlen(optarg) + 1);
                     if (NULL == filename) {
-                        printf("Internal error!\nApplication terminated!\n");
+                        printf("Internal error\nApplication terminated\n");
 #ifdef _DEBUG
                         fprintf(stderr, "Can't allocate memory to store filename string\n");
 #endif
                         goto END; 
                     }
                     strcpy(filename, optarg);
+                    filename_opt_set = true;
                     break;
                 case 'm':
-                    printf("month: %s\n", optarg);
+                    if (month_opt_set) {
+                        printf("-m option can not be set multiple times\nApplication terminated\n");
+                        help_opt_set = true;
+                        goto END;
+                    }
+                    if (1 != sscanf(optarg, "%d", &month_num) || 1 > month_num || 12 < month_num) {
+                        printf("Month should be the number between 1 and 12\nApplication terminated\n");
+#ifdef _DEBUG
+                        fprintf(stderr, "DEBUG: error in month option {%s}\n", optarg);
+#endif
+                        help_opt_set = true;
+                        goto END;
+                    }
+                    month_opt_set = true;
                     break;
             }
         }
     }
 
-    if (NULL == filename) {
-        printf("Filename is not specified!\nApplication terminated!\n");
+    if (!filename_opt_set) {
+        printf("Filename is not specified\nApplication terminated\n");
+        help_opt_set = true;
         goto END;
     }
 
     {
         stream = fopen(filename, "r");
         if (NULL == stream) {
-            printf("Can't open file %s.\nApplication terminated.\n", filename);
+            printf("Can't open file {%s}\nApplication terminated\n", filename);
             goto END;
         }
 
@@ -74,7 +93,7 @@ int main(int argc, char **argv) {
 END: {
     if (NULL != filename) free(filename);
     if (NULL != stream) fclose(stream);
-    if (need_print_help) print_help();
+    if (help_opt_set) print_help();
 }
     return 0;
 }
